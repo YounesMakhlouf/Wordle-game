60bdaef36036af96e25d8d5c75e219bd6b5e7cd3
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2023-08-15 22:32:47 +0100

    Made the JS more readable

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 import Swal from 'sweetalert2';
 
-const letterContainers = document.querySelectorAll('.letter');
-const title = document.querySelector('h1');
+const letters = document.querySelectorAll('.letter');
+const gameTitle = document.querySelector('h1');
 const loader = document.querySelector(".lds-roller");
 const timer = document.querySelector(".timer");
 
 const ANSWER_LENGTH = 5;
 const ROUNDS = 6;
-let isLoading = false;
+const level = {
+    "hard": 60, "medium": 300,
+} // Stores the maximum time for each level
+let isLoading = false; // Used to not consider user input while waiting for API requests
 let currentLetters = [];
 let currentLine = 0;
 let expectedAnswer = [];
-let done = false;
-let time = 0;
-let seconds = time % 60;
-let minutes = Math.floor(time / 60);
-let level = {
-    "hard": 60, "medium": 300,
-}
+let done = false; // Used to determine if the game is over or not
+let time = 0; // Used for the timer
+let seconds;
+let minutes;
 
 function displayTimer() {
     timer.innerHTML = `<span class="timer-number"> ${minutes.toString()}</span><span>min</span>
@@ -26,18 +26,21 @@ function displayTimer() {
 
 function decrementTimer() {
     const timerFunction = setInterval(() => {
-        if (time === 0) {
+        if (time) {
+            if (!done) {
+                seconds = time % 60;
+                minutes = Math.floor(time / 60);
+                displayTimer();
+                time--;
+            }
+        } else {
             done = true;
             Swal.fire("Oh no!", ` You lost. The word was "${expectedAnswer.join('')}" `, "error");
             clearInterval(timerFunction);
-        } else {
-            seconds = time % 60;
-            minutes = Math.floor(time / 60);
-            displayTimer();
-            time--;
         }
     }, 1000);
 }
+
 
 async function getWord() {
     toggleLoader(true);
@@ -63,12 +66,12 @@ function getUserInput(e) {
     if (!(done || isLoading)) {
         if (e.key === "Backspace") {
             currentLetters.pop();
-            letterContainers[currentLetters.length + currentLine * ANSWER_LENGTH].innerText = "";
+            letters[currentLetters.length + currentLine * ANSWER_LENGTH].innerText = "";
         }
         if (currentLetters.length !== ANSWER_LENGTH) {
             if (isLetter(e.key)) {
                 currentLetters.push(e.key.toLowerCase());
-                letterContainers[currentLetters.length - 1 + currentLine * ANSWER_LENGTH].innerText = e.key;
+                letters[currentLetters.length - 1 + currentLine * ANSWER_LENGTH].innerText = e.key;
             }
         } else if (e.key === "Enter") {
             validateInput();
@@ -79,11 +82,11 @@ function getUserInput(e) {
 async function validateInput() {
     const valid = await isValid();
     if (valid) {
-        showAnswer();
+        colourLetters();
         currentLine++;
         if (expectedAnswer.join('') === currentLetters.join('')) {
-            title.classList.add("winner");
             done = true;
+            gameTitle.classList.add("winner");
             await Swal.fire("Congratulations", "Word by word, you conquered the board – a true Wordle maestro!", "success");
         } else if (currentLine === ROUNDS) {
             done = true;
@@ -91,24 +94,24 @@ async function validateInput() {
         }
         currentLetters = [];
     } else {
-        makInvalidWord();
+        markInvalidWord();
     }
 }
 
-function makInvalidWord() {
+function markInvalidWord() {
     for (let i = currentLine * ANSWER_LENGTH; i < (currentLine + 1) * ANSWER_LENGTH; i++) {
-        letterContainers[i].classList.remove("wrong-word");
+        letters[i].classList.remove("wrong-word");
         setTimeout(() => {
-            letterContainers[i].classList.add("wrong-word");
-        }, 10)
+            letters[i].classList.add("wrong-word");
+        }, 10);
     }
 }
 
-function showAnswer() {
+function colourLetters() {
     const map = countLetters(expectedAnswer);
     for (let i = 0; i < ANSWER_LENGTH; i++) {
         if (expectedAnswer[i] === currentLetters[i]) {
-            letterContainers[i + currentLine * ANSWER_LENGTH].classList.add("right-place");
+            letters[i + currentLine * ANSWER_LENGTH].classList.add("right-place");
             map[expectedAnswer[i]]--;
         }
     }
@@ -116,10 +119,10 @@ function showAnswer() {
         if (expectedAnswer[i] === currentLetters[i]) {
             // pass
         } else if (expectedAnswer.includes(currentLetters[i]) && map[currentLetters[i]] > 0) {
-            letterContainers[i + currentLine * ANSWER_LENGTH].classList.add("wrong-place");
+            letters[i + currentLine * ANSWER_LENGTH].classList.add("wrong-place");
             map[expectedAnswer[i]]--;
         } else {
-            letterContainers[i + currentLine * ANSWER_LENGTH].classList.add("wrong");
+            letters[i + currentLine * ANSWER_LENGTH].classList.add("wrong");
         }
     }
 }
@@ -134,7 +137,6 @@ function toggleLoader(loading) {
 }
 
 async function init(difficulty) {
-    console.log(time);
     window.addEventListener("keydown", getUserInput);
     await getWord();
     if (difficulty !== "easy") {
